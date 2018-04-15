544edeaad1ed795e8ea2a483add1801dfed860e7
cheesedev <vasyach1997@yandex.ru>
2018-04-15 22:42:23 +0700

    dasda
    dasda

---
+++ website/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#carousel").owlCarousel();
+    nav: true;
+});
+
+// $(document).ready(function(){
+//     $(".btn").click(function(){
+//         $("i").removeClass("active");
+//         $(this.i).addClass("active");
+//     });
+// });
